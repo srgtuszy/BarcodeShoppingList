2674f1be7804caaeb428921c74970d6f366d7f84
Michał Tuszynski <srgtuszy@gmail.com>
2014-11-15 12:18:47 +0100

    Added core data boilerplate code

--- BarcodeShopping/MainViewController.swift
+++ BarcodeShopping/MainViewController.swift
@@ -9,9 +9,29 @@
 import Foundation
 import UIKit
 
-class MainViewController : BaseViewController {
+class MainViewController : BaseViewController, ZBarReaderDelegate {
+    @IBOutlet weak var tableView: UITableView!
     
+    //MARK: UIViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //MARK: IBActions
+    @IBAction func scanItem() {
+        let reader = ZBarReaderViewController()
+        reader.readerDelegate = self
+        reader.scanner.setSymbology(ZBAR_I25, config: ZBAR_CFG_ENABLE, to: 0)
+        showViewController(reader, sender: self)
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+    }
+    
+    //MARK: ZBarReaderDelegate
+    func readerControllerDidFailToRead(reader: ZBarReaderController!, withRetry retry: Bool) {
+        
     }
 }
---
+++ BarcodeShoppingKit/CoreDataManager.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataManager.swift
+//  BarcodeShopping
+//
+//  Created by Michał Tuszyński on 15/11/14.
+//  Copyright (c) 2014 iapp. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+var sharedInstance: CoreDataManager?
+
+public class CoreDataManager {
+    public var mainContext: NSManagedObjectContext
+    
+    public class func createDefaultStack(persistentStoreURL: NSURL) {
+        let modelURL = NSBundle.mainBundle().URLForResource("ShoppingListModel", withExtension: "momd")
+        let model = NSManagedObjectModel(contentsOfURL: modelURL!)
+        let storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: model!)
+        let persistentStoreOptions = [
+            NSMigratePersistentStoresAutomaticallyOption : true,
+            NSInferMappingModelAutomaticallyOption : true
+        ]
+        var error: NSError?
+        storeCoordinator.addPersistentStoreWithType(NSSQLiteStoreType,
+            configuration: nil,
+            URL: persistentStoreURL,
+            options: persistentStoreOptions,
+            error: &error)
+        if let error = error {
+            fatalError("Could not initialize persistent store: \(error.localizedDescription)")
+        }
+        let context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        context.persistentStoreCoordinator = storeCoordinator
+        sharedInstance = CoreDataManager(mainContext: context)
+    }
+    
+    public class func sharedManager() -> CoreDataManager {
+        return sharedInstance!
+    }
+    
+    init(mainContext: NSManagedObjectContext) {
+        self.mainContext = mainContext
+    }
+    
+}

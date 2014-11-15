5ec707007b5f3f47ae53e639858062c46c65762c
Michał Tuszynski <srgtuszy@gmail.com>
2014-11-15 11:09:56 +0100

    Added shared embedded framework target

---
+++ BarcodeShoppingKit/BarcodeShoppingKit.h
@@ -0,0 +1,19 @@
+//
+//  BarcodeShoppingKit.h
+//  BarcodeShoppingKit
+//
+//  Created by Michał Tuszyński on 15/11/14.
+//  Copyright (c) 2014 iapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BarcodeShoppingKit.
+FOUNDATION_EXPORT double BarcodeShoppingKitVersionNumber;
+
+//! Project version string for BarcodeShoppingKit.
+FOUNDATION_EXPORT const unsigned char BarcodeShoppingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarcodeShoppingKit/PublicHeader.h>
+
+

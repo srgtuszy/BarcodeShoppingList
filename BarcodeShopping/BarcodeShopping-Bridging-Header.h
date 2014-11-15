f98b479d4731e128518fcaae4d8a20d47d3b1dde
Michał Tuszynski <srgtuszy@gmail.com>
2014-11-15 11:29:33 +0100

    Added cocapods and zbar sdks

---
+++ BarcodeShopping/BarcodeShopping-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BarcodeShopping-Bridging-Header.h
+//  BarcodeShopping
+//
+//  Created by Michał Tuszyński on 15/11/14.
+//  Copyright (c) 2014 iapp. All rights reserved.
+//
+
+#ifndef BarcodeShopping_BarcodeShopping_Bridging_Header_h
+#define BarcodeShopping_BarcodeShopping_Bridging_Header_h
+
+#import <ZBarSDK/ZBarSDK.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+xcodeproj 'BarcodeShopping.xcodeproj'
+
+target :BarcodeShopping do
+	platform :ios, '8.1'
+	pod 'ZBarSDK', '~>1.3.1'	
+end
